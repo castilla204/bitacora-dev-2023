43b7ca7a80f71430f6f71d541d84cd388b33c391
Diego Castilla <castilla204@gmail.com>
2023-02-02 20:31:44 +0100

    practica del finde

---
+++ 2023/04-javascript/ejercicio_04.js
@@ -0,0 +1,5 @@
+# JS en el navegador — localstorage
+
+Fecha: 2023-02-02
+
+Practica personal de Diego Castilla.
--- 2023/04-javascript/notas.md
+++ 2023/04-javascript/notas.md
@@ -1,3 +1,3 @@
 # JS en el navegador
 
-Progreso: 3/6 ejercicios.
+Progreso: 4/6 ejercicios.
