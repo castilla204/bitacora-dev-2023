ce7da4948e0523c6af8933474ffdd4187ac0ff13
Diego Castilla <castilla204@gmail.com>
2023-02-13 15:57:23 +0100

    05-sql: insert update

---
+++ 2023/05-sql/ejercicio_04.sql
@@ -0,0 +1,5 @@
+# Bases de datos — insert update
+
+Fecha: 2023-02-13
+
+Practica personal de Diego Castilla.
--- 2023/05-sql/notas.md
+++ 2023/05-sql/notas.md
@@ -1,3 +1,3 @@
 # Bases de datos
 
-Progreso: 3/6 ejercicios.
+Progreso: 4/6 ejercicios.
