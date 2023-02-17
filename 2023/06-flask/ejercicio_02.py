11d167bd41fd225d0a583cf1e2ca38ca1bb856d3
Diego Castilla <castilla204@gmail.com>
2023-02-17 13:09:10 +0100

    notas de clase

---
+++ 2023/06-flask/ejercicio_02.py
@@ -0,0 +1,5 @@
+# Backend Python — rutas dinamicas
+
+Fecha: 2023-02-17
+
+Practica personal de Diego Castilla.
--- 2023/06-flask/notas.md
+++ 2023/06-flask/notas.md
@@ -1,3 +1,3 @@
 # Backend Python
 
-Progreso: 1/6 ejercicios.
+Progreso: 2/6 ejercicios.
