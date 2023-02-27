2946171f18b2e0da4338639a4675485f2c2b0c39
Diego Castilla <castilla204@gmail.com>
2023-02-27 14:46:46 +0100

    07-pandas: matplotlib grafico

---
+++ 2023/07-pandas/ejercicio_04.py
@@ -0,0 +1,5 @@
+# Analisis de datos — matplotlib grafico
+
+Fecha: 2023-02-27
+
+Practica personal de Diego Castilla.
--- 2023/07-pandas/notas.md
+++ 2023/07-pandas/notas.md
@@ -1,3 +1,3 @@
 # Analisis de datos
 
-Progreso: 3/6 ejercicios.
+Progreso: 4/6 ejercicios.
