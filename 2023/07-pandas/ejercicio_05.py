e5dba5b547d93b74f15ac262a50aa07bffe1f411
Diego Castilla <castilla204@gmail.com>
2023-02-28 18:12:42 +0100

    07-pandas: limpieza nulos

---
+++ 2023/07-pandas/ejercicio_05.py
@@ -0,0 +1,5 @@
+# Analisis de datos — limpieza nulos
+
+Fecha: 2023-02-28
+
+Practica personal de Diego Castilla.
--- 2023/07-pandas/notas.md
+++ 2023/07-pandas/notas.md
@@ -1,3 +1,3 @@
 # Analisis de datos
 
-Progreso: 4/6 ejercicios.
+Progreso: 5/6 ejercicios.
